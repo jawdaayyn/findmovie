91277adcf796a2044c1365e40b597b3143c31f35
unknown <akkari_j@etna-alternance.net>
2022-12-20 05:09:24 +0100

    added list of movies on sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,66 +1,34 @@
 import Link from "next/link";
-
-const SideBar = () => {
+import { InferGetServerSidePropsType } from "next";
+import { getServerSideProps } from "../pages";
+const SideBar = ({
+  list,
+  list2,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <aside className="w-64 " aria-label="Sidebar">
-      <div className="flex h-screen overflow-y-auto py-4 px-3 bg-gray-50 rounded-none dark:bg-gray-800">
+      <div className="flex scrollbar-hide h-screen overflow-y-auto py-4 px-3 bg-gray-50 rounded-none dark:bg-gray-800">
         <ul className="space-y-2">
-          <li>
-            <Link
-              href="#"
-              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-none dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="ml-3">Dashboard</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#"
-              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-none dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="flex-1 ml-3 whitespace-nowrap">Kanban</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#"
-              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-none dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="flex-1 ml-3 whitespace-nowrap">Inbox</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#"
-              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-none dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#"
-              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-none dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="flex-1 ml-3 whitespace-nowrap">Products</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#"
-              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-none dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="flex-1 ml-3 whitespace-nowrap">Sign In</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#"
-              className="flex items-center p-2 text-base font-normal text-gray-900 rounded-none dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-            >
-              <span className="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
-            </Link>
-          </li>
+          {list?.Search.map((movie: any) => (
+            <li>
+              <Link
+                href="#"
+                className="flex items-center p-2 text-base font-normal text-gray-900 rounded-none dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <span className="ml-3">{movie.Title}</span>
+              </Link>
+            </li>
+          ))}
+          {list2?.Search.map((movie: any) => (
+            <li>
+              <Link
+                href="#"
+                className="flex items-center p-2 text-base font-normal text-gray-900 rounded-none dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <span className="ml-3">{movie.Title}</span>
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </aside>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,14 @@ import { InferGetServerSidePropsType } from "next";
 
 function Home({
   movie,
+  list,
+  list2,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(movie);
   return (
     <div className="flex-col">
       <Nav />
       <div className="flex">
-        <SideBar />
+        <SideBar list={list} list2={list2} movie={undefined} />
 
         <ul>
           <img src={movie.Poster} />
@@ -44,10 +45,19 @@ export async function getServerSideProps() {
   const key = process.env["KEY"];
   const res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${key}`);
   const movie: any = await res.json();
-
+  const resList = await fetch(
+    `https://omdbapi.com/?type=movie&page=1&apikey=${key}&s=love`
+  );
+  const list: any = await resList.json();
+  const resList2 = await fetch(
+    `https://omdbapi.com/?type=movie&page=1&apikey=${key}&s=car`
+  );
+  const list2: any = await resList2.json();
   // Pass data to the page via props
   return {
     props: {
+      list,
+      list2,
       movie,
     },
   };
